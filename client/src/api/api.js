65765f63c1f140f864962cb8c30cf6943b2cc9c5
bersat <doom_fox754@mail.ru>
2025-05-31 23:31:12 +0300

    повторыне изменения API_URL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Устанавливаем базовый URL для API
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 // Получение всех категорий
 export const getCategories = async () => {
